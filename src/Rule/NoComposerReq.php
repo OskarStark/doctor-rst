77425e2bd7e246f0fe7d5792d340204612f20ecd
Oskar Stark <oskarstark@googlemail.com>
2019-03-10 15:52:25 +0100

    composer req rule. fixes #8

---
+++ src/Rule/NoComposerReq.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of DOCtor-RST.
+ *
+ * (c) Oskar Stark <oskarstark@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Rule;
+
+use App\Handler\RulesHandler;
+use App\Rst\RstParser;
+
+class NoComposerReq implements Rule
+{
+    public static function getName(): string
+    {
+        return 'no_composer_req';
+    }
+
+    public static function getGroups(): array
+    {
+        return [RulesHandler::GROUP_SYMFONY];
+    }
+
+    public function check(\ArrayIterator $lines, int $number)
+    {
+        $lines->seek($number);
+        $line = $lines->current();
+
+        $line = RstParser::clean($line);
+        if (preg_match('/composer req /', $line)) {
+            return 'Please "composer require" instead of "composer req"';
+        }
+    }
+}
---
+++ tests/Rule/NoComposerReqTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of DOCtor-RST.
+ *
+ * (c) Oskar Stark <oskarstark@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace app\tests\Rule;
+
+use App\Rule\NoComposerReq;
+use PHPUnit\Framework\TestCase;
+
+class NoComposerReqTest extends TestCase
+{
+    /**
+     * @test
+     *
+     * @dataProvider checkProvider
+     */
+    public function check($expected, $line)
+    {
+        $this->assertSame(
+            $expected,
+            (new NoComposerReq())->check(new \ArrayIterator([$line]), 0)
+        );
+    }
+
+    public function checkProvider()
+    {
+        return [
+            [
+                'Please "composer require" instead of "composer req"',
+                'composer req symfony/form',
+            ],
+            [
+                null,
+                'composer require symfony/form',
+            ],
+        ];
+    }
+}
