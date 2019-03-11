93c800af03b78970ae0f09926965ed9f7dcb8a7f
Oskar Stark <oskarstark@googlemail.com>
2019-03-11 09:37:25 +0100

    CS fix

--- tests/Rst/RstParserTest.php
+++ tests/Rst/RstParserTest.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace app\tests\Rst;
+namespace App\Tests\Rst;
 
 use App\Rst\RstParser;
 use PHPUnit\Framework\TestCase;
--- tests/Rule/NoBlankLineAfterFilepathInCodeBlockTest.php
+++ tests/Rule/NoBlankLineAfterFilepathInCodeBlockTest.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace app\tests\Rule;
+namespace App\Tests\Rule;
 
 use App\Rule\NoBlankLineAfterFilepathInCodeBlock;
 use PHPUnit\Framework\TestCase;
