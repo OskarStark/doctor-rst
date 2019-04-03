edc69b4b96fbda44a8308b80baffd9c3b7cfa18d
Oskar Stark <oskarstark@googlemail.com>
2019-04-03 21:43:55 +0200

    added new rule: lowercase_as_in_use_statements

---
+++ src/Rule/LowercaseAsInUseStatements.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of DOCtor-RST.
+ *
+ * (c) Oskar Stark <oskarstark@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Rule;
+
+use App\Handler\RulesHandler;
+use App\Rst\RstParser;
+
+class LowercaseAsInUseStatements extends AbstractRule implements Rule
+{
+    public static function getGroups(): array
+    {
+        return [RulesHandler::GROUP_SONATA, RulesHandler::GROUP_SYMFONY];
+    }
+
+    public function check(\ArrayIterator $lines, int $number)
+    {
+        $lines->seek($number);
+        $line = $lines->current();
+
+        if (!RstParser::codeBlockDirectiveIsTypeOf($line, RstParser::CODE_BLOCK_PHP)
+            && !RstParser::codeBlockDirectiveIsTypeOf($line, RstParser::CODE_BLOCK_PHP_ANNOTATIONS)
+        ) {
+            return;
+        }
+
+        $indention = RstParser::indention($line);
+
+        $lines->next();
+
+        while ($lines->valid()
+            && !RstParser::isDirective($lines->current())
+            && ($indention < RstParser::indention($lines->current()) || RstParser::isBlankLine($lines->current()))
+        ) {
+            if (preg_match('/^use (.*) (AS|As|aS) (.*);$/', RstParser::clean($lines->current()), $matches)) {
+                return sprintf('Please use lowercase "as" instead of "%s"', $matches[2]);
+            }
+
+            $lines->next();
+        }
+    }
+}
---
+++ tests/Rule/LowercaseAsInUseStatementTest.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of DOCtor-RST.
+ *
+ * (c) Oskar Stark <oskarstark@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Rule;
+
+use App\Rule\LowercaseAsInUseStatements;
+use App\Tests\RstSample;
+use PHPUnit\Framework\TestCase;
+
+class LowercaseAsInUseStatementTest extends TestCase
+{
+    /**
+     * @test
+     *
+     * @dataProvider checkProvider
+     */
+    public function check($expected, RstSample $sample)
+    {
+        $this->assertSame(
+            $expected,
+            (new LowercaseAsInUseStatements())->check($sample->getContent(), $sample->getLineNumber())
+        );
+    }
+
+    public function checkProvider(): \Generator
+    {
+        $codeBlocks = [
+            '.. code-block:: php',
+            '.. code-block:: php-annotations',
+            'A php code block follows::',
+        ];
+
+        // VALID
+        foreach ($codeBlocks as $codeBlock) {
+            // WITH blank line after directive
+            yield [
+                null,
+                new RstSample([
+                    $codeBlock,
+                    '',
+                    '    use Symfony\A as A;',
+                ]),
+            ];
+
+            // WITHOUT blank line after directive
+            yield [
+                null,
+                new RstSample([
+                    $codeBlock,
+                    '    use Symfony\A as A;',
+                ]),
+            ];
+        }
+
+        // INVALID
+        foreach ($codeBlocks as $codeBlock) {
+            foreach (['AS', 'As', 'aS'] as $invalid) {
+                // WITH blank line after directive
+                yield [
+                    sprintf('Please use lowercase "as" instead of "%s"', $invalid),
+                    new RstSample([
+                        $codeBlock,
+                        '',
+                        sprintf('    use Symfony\A %s A;', $invalid),
+                    ]),
+                ];
+
+                // WITHOUT blank line after directive
+                yield [
+                    sprintf('Please use lowercase "as" instead of "%s"', $invalid),
+                    new RstSample([
+                        $codeBlock,
+                        sprintf('    use Symfony\A %s A;', $invalid),
+                    ]),
+                ];
+            }
+        }
+    }
+}
