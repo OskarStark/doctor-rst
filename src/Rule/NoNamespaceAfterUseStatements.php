7b1b4c94ad57a2f9acc0eda1d5b87bdea76e810b
Oskar Stark <oskarstark@googlemail.com>
2019-04-04 08:36:52 +0200

    new rule: no_namespace_after_use_statement

---
+++ src/Rule/NoNamespaceAfterUseStatements.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of DOCtor-RST.
+ *
+ * (c) Oskar Stark <oskarstark@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Rule;
+
+use App\Handler\RulesHandler;
+use App\Rst\RstParser;
+
+class NoNamespaceAfterUseStatements extends AbstractRule implements Rule
+{
+    public static function getGroups(): array
+    {
+        return [RulesHandler::GROUP_SONATA, RulesHandler::GROUP_SYMFONY];
+    }
+
+    public function check(\ArrayIterator $lines, int $number)
+    {
+        $lines->seek($number);
+        $line = $lines->current();
+
+        if (!RstParser::codeBlockDirectiveIsTypeOf($line, RstParser::CODE_BLOCK_PHP)
+            && !RstParser::codeBlockDirectiveIsTypeOf($line, RstParser::CODE_BLOCK_PHP_ANNOTATIONS)
+        ) {
+            return;
+        }
+
+        $indention = RstParser::indention($line);
+
+        $lines->next();
+
+        $useStatementFound = false;
+
+        while ($lines->valid()
+            && !RstParser::isDirective($lines->current())
+            && ($indention < RstParser::indention($lines->current()) || RstParser::isBlankLine($lines->current()))
+        ) {
+            if (preg_match('/^use (.*);$/', RstParser::clean($lines->current()), $matches)) {
+                $useStatementFound = true;
+            }
+
+            if (preg_match('/^namespace (.*);$/', RstParser::clean($lines->current()), $matches)) {
+                if ($useStatementFound) {
+                    return 'Please move the namespace before the use statement(s)';
+                }
+                break;
+            }
+
+            $lines->next();
+        }
+    }
+}
---
+++ tests/Rule/NoNamespaceAfterUseStatementsTest.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of DOCtor-RST.
+ *
+ * (c) Oskar Stark <oskarstark@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Rule;
+
+use App\Rule\NoNamespaceAfterUseStatements;
+use App\Tests\RstSample;
+use PHPUnit\Framework\TestCase;
+
+class NoNamespaceAfterUseStatementsTest extends TestCase
+{
+    /**
+     * @test
+     *
+     * @dataProvider checkProvider
+     */
+    public function check($expected, RstSample $sample)
+    {
+        $this->assertSame(
+            $expected,
+            (new NoNamespaceAfterUseStatements())->check($sample->getContent(), $sample->getLineNumber())
+        );
+    }
+
+    public function checkProvider(): \Generator
+    {
+        $codeBlocks = [
+            '.. code-block:: php',
+            '.. code-block:: php-annotations',
+            'A php code block follows::',
+        ];
+
+        // VALID
+        foreach ($codeBlocks as $codeBlock) {
+            // WITH blank line after directive
+            yield [
+                null,
+                new RstSample([
+                    $codeBlock,
+                    '',
+                    '    namespace App;',
+                    '    use Symfony\A;',
+                ]),
+            ];
+
+            // WITHOUT blank line after directive
+            yield [
+                null,
+                new RstSample([
+                    $codeBlock,
+                    '    namespace App;',
+                    '    use Symfony\A;',
+                ]),
+            ];
+        }
+
+        // INVALID
+        foreach ($codeBlocks as $codeBlock) {
+            // WITH blank line after directive
+            yield [
+                'Please move the namespace before the use statement(s)',
+                new RstSample([
+                    $codeBlock,
+                    '',
+                    '    use Symfony\A;',
+                    '    namespace App;',
+                ]),
+            ];
+
+            // WITHOUT blank line after directive
+            yield [
+                'Please move the namespace before the use statement(s)',
+                new RstSample([
+                    $codeBlock,
+                    '    use Symfony\A;',
+                    '    namespace App;',
+                ]),
+            ];
+        }
+    }
+}
