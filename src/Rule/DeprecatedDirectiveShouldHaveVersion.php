1a7bf7ffc5c32b543a7c40bf39f8e3ea4a5e17f4
Oskar Stark <oskarstark@googlemail.com>
2019-04-25 12:01:24 +0200

    new rule: deprecated_directive_should_have_version

---
+++ src/Rule/DeprecatedDirectiveShouldHaveVersion.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of DOCtor-RST.
+ *
+ * (c) Oskar Stark <oskarstark@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Rule;
+
+use App\Annotations\Rule\Description;
+use App\Annotations\Rule\InvalidExample;
+use App\Annotations\Rule\ValidExample;
+use App\Handler\RulesHandler;
+use App\Rst\RstParser;
+use Composer\Semver\VersionParser;
+
+/**
+ * @Description("Ensure a deprecated directive has a version which follows SemVer.")
+ * @ValidExample(".. deprecated:: 3.4")
+ * @InvalidExample({".. deprecated::", ".. deprecated:: foo-bar"})
+ */
+class DeprecatedDirectiveShouldHaveVersion extends AbstractRule implements Rule
+{
+    /** @var VersionParser */
+    private $versionParser;
+
+    public function __construct(VersionParser $versionParser)
+    {
+        $this->versionParser = $versionParser;
+    }
+
+    public static function getGroups(): array
+    {
+        return [RulesHandler::GROUP_SONATA, RulesHandler::GROUP_SYMFONY];
+    }
+
+    public function check(\ArrayIterator $lines, int $number)
+    {
+        $lines->seek($number);
+        $line = $lines->current();
+
+        if (!RstParser::directiveIs($line, RstParser::DIRECTIVE_DEPRECATED)) {
+            return;
+        }
+
+        if (preg_match(sprintf('/^%s(.*)$/', RstParser::DIRECTIVE_DEPRECATED), RstParser::clean($lines->current()), $matches)) {
+            $version = trim($matches[1]);
+
+            if (empty($version)) {
+                return sprintf('Please provide a version behind "%s"', RstParser::DIRECTIVE_DEPRECATED);
+            }
+
+            try {
+                $this->versionParser->normalize($version);
+            } catch (\UnexpectedValueException $e) {
+                return sprintf(
+                    'Please provide a numeric version behind "%s" instead of "%s"',
+                    RstParser::DIRECTIVE_DEPRECATED,
+                    $version
+                );
+            }
+        }
+    }
+}
---
+++ tests/Rule/DeprecatedDirectiveShouldHaveVersionTest.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of DOCtor-RST.
+ *
+ * (c) Oskar Stark <oskarstark@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Rule;
+
+use App\Rule\DeprecatedDirectiveShouldHaveVersion;
+use App\Tests\RstSample;
+use Composer\Semver\VersionParser;
+use PHPUnit\Framework\TestCase;
+
+class DeprecatedDirectiveShouldHaveVersionTest extends TestCase
+{
+    /**
+     * @test
+     *
+     * @dataProvider checkProvider
+     */
+    public function check($expected, RstSample $sample)
+    {
+        $this->assertSame(
+            $expected,
+            (new DeprecatedDirectiveShouldHaveVersion(new VersionParser()))
+                ->check($sample->getContent(), $sample->getLineNumber())
+        );
+    }
+
+    public function checkProvider()
+    {
+        return [
+            [
+                null,
+                new RstSample('.. deprecated:: 1'),
+            ],
+            [
+                null,
+                new RstSample('.. deprecated:: 1.2'),
+            ],
+            [
+                null,
+                new RstSample('.. deprecated:: 1.2.0'),
+            ],
+            [
+                null,
+                new RstSample('.. deprecated:: 1.2   '),
+            ],
+            [
+                'Please provide a version behind ".. deprecated::"',
+                new RstSample('.. deprecated::'),
+            ],
+            [
+                'Please provide a numeric version behind ".. deprecated::" instead of "foo"',
+                new RstSample('.. deprecated:: foo'),
+            ],
+        ];
+    }
+}
