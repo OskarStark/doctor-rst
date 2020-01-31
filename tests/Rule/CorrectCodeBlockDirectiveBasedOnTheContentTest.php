aa497631f47951fc69beb7d2c6c3c7a254b4809b
Oskar Stark <oskarstark@googlemail.com>
2020-01-31 11:09:17 +0100

    Revert "Fix regression and use less regex magic"
    This reverts commit 7aab5f35b95539cd7ae765ae30ab6cfbb1e7eec9.

--- tests/Rule/CorrectCodeBlockDirectiveBasedOnTheContentTest.php
+++ tests/Rule/CorrectCodeBlockDirectiveBasedOnTheContentTest.php
@@ -22,84 +22,6 @@ class CorrectCodeBlockDirectiveBasedOnTheContentTest extends TestCase
     /**
      * @test
      *
-     * @dataProvider containsHtmlProvider
-     */
-    public function containsHtml(bool $expected, RstSample $sample)
-    {
-        $this->assertSame(
-            $expected,
-            (new CorrectCodeBlockDirectiveBasedOnTheContent())->containsHtml($sample->lines()->toIterator(), 0)
-        );
-    }
-
-    /**
-     * @return \Generator<array{0: bool, 1: RstSample}>
-     */
-    public function containsHtmlProvider(): \Generator
-    {
-        yield [
-            false,
-            new RstSample(
-                <<<CONTENT
-{# templates/admin/tag_collection.html.twig #}
-{% set colors = field_options.label_colors|default(['primary']) %}
-
-{% for tag in value %}
-    I love you <3
-{% endfor %}
-CONTENT
-            ),
-        ];
-
-        yield [
-            true,
-            new RstSample(
-                <<<CONTENT
-{# templates/admin/tag_collection.html.twig #}
-{% set colors = field_options.label_colors|default(['primary']) %}
-
-{% for tag in value %}
-    <span class="label label-{{ cycle(colors, loop.index) }}">{{ tag }}</span>
-{% endfor %}
-CONTENT
-            ),
-        ];
-
-        yield [
-            false,
-            new RstSample(
-                <<<CONTENT
-{# templates/admin/restock_action.html.twig #}
-
-{# if the stock is low, include the default action template to render the
-   action link. Otherwise, don't include the template so the link is not displayed #}
-{% if item.stock < 10 %}
-    {{ include('@EasyAdmin/default/action.html.twig') }}
-{% endif %}
-CONTENT
-            ),
-        ];
-
-        yield [
-            false,
-            new RstSample(
-                <<<CONTENT
-    {# templates/bundles/EasyAdminBundle/default/field_string.html.twig #}
-    {% if field_options.trans|default(false) %}
-        {# translate fields defined as "translatable" #}
-        {{ value|trans({}, field_options.domain|default('messages')) }}
-    {% else %}
-        {# if not translatable, simply include the default template #}
-        {{ include('@!EasyAdmin/default/field_string.html.twig') }}
-    {% endif %}
-CONTENT
-            ),
-        ];
-    }
-
-    /**
-     * @test
-     *
      * @dataProvider checkProvider
      */
     public function check(?string $expected, RstSample $sample)
@@ -130,53 +52,6 @@ CONTENT
 CONTENT
                 ),
             ];
-
-        yield [
-            null,
-            new RstSample(<<<CONTENT
-.. code-block:: twig
-
-    {# templates/bundles/EasyAdminBundle/default/field_string.html.twig #}
-    {% if field_options.trans|default(false) %}
-        {# translate fields defined as "translatable" #}
-        {{ value|trans({}, field_options.domain|default('messages')) }}
-    {% else %}
-        {# if not translatable, simply include the default template #}
-        {{ include('@!EasyAdmin/default/field_string.html.twig') }}
-    {% endif %}
-CONTENT
-            ),
-        ];
-
-        yield [
-            null,
-            new RstSample(<<<CONTENT
-.. code-block:: twig
-
-    {# templates/admin/tag_collection.html.twig #}
-    {% set colors = field_options.label_colors|default(['primary']) %}
-
-    {% for tag in value %}
-        I love you <3
-    {% endfor %}
-CONTENT
-            ),
-        ];
-
-        yield [
-            null,
-            new RstSample(<<<CONTENT
-.. code-block:: html+twig
-
-    {# templates/admin/tag_collection.html.twig #}
-    {% set colors = field_options.label_colors|default(['primary']) %}
-
-    {% for tag in value %}
-        <span class="label label-{{ cycle(colors, loop.index) }}">{{ tag }}</span>
-    {% endfor %}
-CONTENT
-            ),
-        ];
 
         yield [
             'Please use "twig" instead of "html+twig"',
