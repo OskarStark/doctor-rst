cf423326c4a8af0506b647d8b563decb31f1b709
Oskar Stark <oskarstark@googlemail.com>
2020-10-06 11:08:35 +0200

    Enhancement: New rule - NoBracketsInMethodDirective
    Fixes #495

---
+++ tests/Rule/NoBracketsInMethodDirectiveTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of DOCtor-RST.
+ *
+ * (c) Oskar Stark <oskarstark@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Rule;
+
+use App\Rule\NoBracketsInMethodDirective;
+use App\Tests\RstSample;
+use PHPUnit\Framework\TestCase;
+
+class NoBracketsInMethodDirectiveTest extends TestCase
+{
+    /**
+     * @test
+     *
+     * @dataProvider checkProvider
+     */
+    public function check(?string $expected, RstSample $sample)
+    {
+        static::assertSame(
+            $expected,
+            (new NoBracketsInMethodDirective())->check($sample->lines(), $sample->lineNumber())
+        );
+    }
+
+    /**
+     * @return \Generator<array{0: string|null, 1: RstSample}>
+     */
+    public function checkProvider(): \Generator
+    {
+        yield [
+            'Please remove "()" inside :method: directive',
+            new RstSample(':method:`Symfony\\Component\\OptionsResolver\\Options::offsetGet()`'),
+        ];
+
+        yield [
+            null,
+            new RstSample(':method:`Symfony\\Component\\OptionsResolver\\Options::offsetGet`'),
+        ];
+    }
+}
