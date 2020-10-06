a6bcddc57734386be62b152a1f33cb76c7ed05af
Oskar Stark <oskarstark@googlemail.com>
2020-10-06 12:18:59 +0200

    Enhancement: New rule - SpaceBetweenLabelAndLinkInRef
    Fixes #494

---
+++ src/Rule/SpaceBetweenLabelAndLinkInRef.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of DOCtor-RST.
+ *
+ * (c) Oskar Stark <oskarstark@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Rule;
+
+use App\Annotations\Rule\Description;
+use App\Annotations\Rule\InvalidExample;
+use App\Annotations\Rule\ValidExample;
+use App\Handler\Registry;
+use App\Value\Lines;
+use App\Value\RuleGroup;
+use function Symfony\Component\String\u;
+
+/**
+ * @Description("Ensure a space between label and link in :ref: directive.")
+ * @InvalidExample(":ref:`receiving them via a worker<messenger-worker>`")
+ * @ValidExample(":ref:`receiving them via a worker <messenger-worker>`")
+ */
+class SpaceBetweenLabelAndLinkInRef extends AbstractRule implements Rule
+{
+    public static function getGroups(): array
+    {
+        return [
+            RuleGroup::fromString(Registry::GROUP_SONATA),
+            RuleGroup::fromString(Registry::GROUP_SYMFONY),
+        ];
+    }
+
+    public function check(Lines $lines, int $number): ?string
+    {
+        $lines->seek($number);
+        $line = $lines->current()->raw();
+
+        if ($matches = u($line)->match('/:ref:`(?P<label>.*)<(?P<link>.*)>`/')) {
+            if (!u($matches['label'])->endsWith(' ')) {
+                return sprintf(
+                    'Please add a space between "%s" and "<%s>" inside :ref: directive',
+                    $matches['label'],
+                    $matches['link']
+                );
+            }
+        }
+
+        return null;
+    }
+}
