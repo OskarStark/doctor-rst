49dc098ddd5c7ec44c0d7d7b81b6687f4d5d4089
Antoine Lamirault <lantoine37@hotmail.fr>
2023-01-12 08:00:59 +0100

    New rule: Argument variable must match type (#1233)

---
+++ src/Rule/ArgumentVariableMustMatchType.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of DOCtor-RST.
+ *
+ * (c) Oskar Stark <oskarstark@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Rule;
+
+use App\Annotations\Rule\Description;
+use App\Rst\RstParser;
+use App\Value\Lines;
+use App\Value\RuleGroup;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * @Description("Make sure argument variable name match for type")
+ */
+class ArgumentVariableMustMatchType extends AbstractRule implements LineContentRule, Configurable
+{
+    /** @var array<array{type: string, name: string}> */
+    private array $arguments;
+
+    public function configureOptions(OptionsResolver $resolver): OptionsResolver
+    {
+        $resolver
+            ->setRequired('arguments')
+            ->setAllowedTypes('arguments', 'array')
+            ->setDefault('arguments', function (OptionsResolver $connResolver): void {
+                $connResolver
+                    ->setPrototype(true)
+                    ->setRequired(['type', 'name']);
+            });
+
+        return $resolver;
+    }
+
+    public function setOptions(array $options): void
+    {
+        $resolver = $this->configureOptions(new OptionsResolver());
+
+        $resolvedOptions = $resolver->resolve($options);
+
+        $this->arguments = $resolvedOptions['arguments'];
+    }
+
+    public static function getGroups(): array
+    {
+        return [RuleGroup::Symfony()];
+    }
+
+    public function check(Lines $lines, int $number): ?string
+    {
+        $lines->seek($number);
+        $line = $lines->current();
+
+        if (!RstParser::codeBlockDirectiveIsTypeOf($line, RstParser::CODE_BLOCK_PHP)
+            && !RstParser::codeBlockDirectiveIsTypeOf($line, RstParser::CODE_BLOCK_PHP_ANNOTATIONS)
+            && !RstParser::codeBlockDirectiveIsTypeOf($line, RstParser::CODE_BLOCK_PHP_ATTRIBUTES)
+        ) {
+            return null;
+        }
+
+        $lines->next();
+        $lines->next();
+
+        $messageParts = [];
+        foreach ($this->arguments as $argument) {
+            // This regex match argument type with bad argument name
+            $regex = sprintf(
+                '/%s \$(?!%s)(?<actualName>[a-z-A-Z\$]+)/',
+                $argument['type'],
+                $argument['name']
+            );
+
+            if ($match = $lines->current()->clean()->match($regex)) {
+                $messageParts[] = sprintf(
+                    'Please rename "$%s" to "$%s"',
+                    $match['actualName'],
+                    $argument['name']
+                );
+            }
+        }
+
+        return $messageParts ? implode('. ', $messageParts) : null;
+    }
+}
