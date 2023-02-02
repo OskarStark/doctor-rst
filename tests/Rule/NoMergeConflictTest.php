0948872205d0bb9cc3fa04926244f22cdaee6d7d
Oskar Stark <oskarstark@googlemail.com>
2023-02-02 06:38:23 +0100

    Enhancement: New rule

---
+++ tests/Rule/NoMergeConflictTest.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of DOCtor-RST.
+ *
+ * (c) Oskar Stark <oskarstark@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Rule;
+
+use App\Rule\LowercaseAsInUseStatements;
+use App\Rule\NoMergeConflict;
+use App\Tests\RstSample;
+use App\Value\NullViolation;
+use App\Value\Violation;
+use App\Value\ViolationInterface;
+
+final class NoMergeConflictTest extends \App\Tests\UnitTestCase
+{
+    /**
+     * @test
+     *
+     * @dataProvider checkProvider
+     */
+    public function check(ViolationInterface $expected, RstSample $sample): void
+    {
+        static::assertEquals(
+            $expected,
+            (new NoMergeConflict())->check($sample->lines(), $sample->lineNumber(), 'filename')
+        );
+    }
+
+    public function checkProvider(): \Generator
+    {
+        yield [
+            NullViolation::create(),
+            new RstSample([
+                '',
+            ]),
+        ];
+
+        yield [
+            Violation::from(
+                'Please get rid of the merge conflict',
+                'filename',
+                1,
+                '<<<<<<< HEAD'
+            ),
+            new RstSample([
+                '<<<<<<< HEAD',
+                '=======',
+                '>>>>>>> 1234567890',
+            ]),
+        ];
+
+        yield [
+            Violation::from(
+                'Please get rid of the merge conflict',
+                'filename',
+                2,
+                '<<<<<<< HEAD'
+            ),
+            new RstSample([
+                '',
+                '<<<<<<< HEAD',
+                '=======',
+                '>>>>>>> 1234567890',
+            ], 1),
+        ];
+    }
+}
