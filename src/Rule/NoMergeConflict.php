4d24ec43bde0b331f68954ee57d7619f72d68038
Oskar Stark <oskarstark@googlemail.com>
2023-02-03 05:59:47 +0100

    Enhancement: New rule `NoMergeConflict` (#1276)

---
+++ src/Rule/NoMergeConflict.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of DOCtor-RST.
+ *
+ * (c) Oskar Stark <oskarstark@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Rule;
+
+use App\Annotations\Rule\Description;
+use App\Value\Lines;
+use App\Value\NullViolation;
+use App\Value\RuleGroup;
+use App\Value\Violation;
+use App\Value\ViolationInterface;
+
+/**
+ * @Description("Ensure that the files does not contain merge conflicts.")
+ */
+final class NoMergeConflict extends AbstractRule implements LineContentRule
+{
+    public static function getGroups(): array
+    {
+        return [RuleGroup::Symfony()];
+    }
+
+    public function check(Lines $lines, int $number, string $filename): ViolationInterface
+    {
+        $lines->seek($number);
+        $line = $lines->current();
+
+        if ($line->clean()->equalsTo('<<<<<<< HEAD')) {
+            return Violation::from(
+                'Please get rid of the merge conflict',
+                $filename,
+                $number + 1,
+                $line->raw()->trim()->toString()
+            );
+        }
+
+        return NullViolation::create();
+    }
+}
