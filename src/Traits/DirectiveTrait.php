ed135af69a64405947737061628b7210b662efa8
Oskar Stark <oskarstark@googlemail.com>
2023-02-08 08:09:56 +0100

    Enhancement: Add `getLineNumberOfDirective()` method

--- src/Traits/DirectiveTrait.php
+++ src/Traits/DirectiveTrait.php
@@ -18,6 +18,46 @@ use App\Value\Lines;
 
 trait DirectiveTrait
 {
+    private function getDirectiveContent(string $directive, Lines $lines, int $number): array
+    {
+        $content = [];
+
+        $number = $this->getLineNumberOfDirective($directive, $lines, $number);
+
+        $lines->seek($number + 1);
+        $startingLine = $lines->current();
+
+        while ($lines->current()->indention() < $startingLine->indention()) {
+            $content[] = $lines->current()->raw();
+
+            $lines->next();
+        }
+
+        return $content;
+    }
+
+    public function getLineNumberOfDirective(string $directive, Lines $lines, int $number): int
+    {
+        $lines->seek($number);
+        $startingLine = $lines->current();
+        while ((
+                $lines->current()->indention() === $startingLine->indention()
+                || $lines->current()->isBlank()
+            ) && !$lines->current()->isDirective()
+        ) {
+            $lines->previous();
+        }
+
+        if ($lines->valid()
+            && $lines->current()->isDirective()
+            && RstParser::directiveIs($lines->current(), $directive)
+        ) {
+            return $lines->key();
+        }
+
+        throw new \RuntimeException(sprintf('Directive "%s" not found', $directive));
+    }
+
     private function inPhpCodeBlock(Lines $lines, int $number): bool
     {
         return $this->in(
