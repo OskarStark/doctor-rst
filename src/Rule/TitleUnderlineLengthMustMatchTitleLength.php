a9996070e2237197abd7ebcd5578aed9b7b5b89e
Matthieu Lempereur <matt.lempereur@gmail.com>
2023-03-08 11:13:13 +0100

    New Rule `TitleUnderlineLengthMustMatchTitleLength` (#1337)

---
+++ src/Rule/TitleUnderlineLengthMustMatchTitleLength.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of DOCtor-RST.
+ *
+ * (c) Oskar Stark <oskarstark@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Rule;
+
+use App\Value\Lines;
+use App\Value\NullViolation;
+use App\Value\Violation;
+use App\Value\ViolationInterface;
+
+class TitleUnderlineLengthMustMatchTitleLength extends AbstractRule implements LineContentRule
+{
+    public function check(Lines $lines, int $number, string $filename): ViolationInterface
+    {
+        $lines->seek($number);
+        $line = $lines->current();
+
+        if (!$line->isHeadline()) {
+            return NullViolation::create();
+        }
+
+        $headLineLength = mb_strlen($line->clean()->toString());
+        $lines->seek($number - 1);
+
+        if ($lines->valid()
+            && !$lines->current()->isBlank()
+            && !(mb_strlen($lines->current()->clean()->toString()) === $headLineLength)
+        ) {
+            return Violation::from(
+                sprintf('Please ensure title "%s" and underline length are matching', $lines->current()->raw()->toString()),
+                $filename,
+                $number,
+                $lines->current()
+            );
+        }
+
+        return NullViolation::create();
+    }
+}
---
+++ tests/Rule/TitleUnderlineLengthMustMatchTitleLengthTest.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of DOCtor-RST.
+ *
+ * (c) Oskar Stark <oskarstark@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Rule;
+
+use App\Rule\TitleUnderlineLengthMustMatchTitleLength;
+use App\Tests\RstSample;
+use App\Value\NullViolation;
+use App\Value\Violation;
+use App\Value\ViolationInterface;
+
+final class TitleUnderlineLengthMustMatchTitleLengthTest extends \App\Tests\UnitTestCase
+{
+    /**
+     * @test
+     *
+     * @dataProvider checkProvider
+     */
+    public function check(ViolationInterface $expected, RstSample $sample): void
+    {
+        static::assertEquals(
+            $expected,
+            (new TitleUnderlineLengthMustMatchTitleLength())->check($sample->lines(), $sample->lineNumber(), 'filename')
+        );
+    }
+
+    public function checkProvider(): array
+    {
+        return [
+            [
+                Violation::from(
+                    sprintf('Please ensure title "%s" and underline length are matching', 'Title with too short underline'),
+                    'filename',
+                    1,
+                    'Title with too short underline'
+                ),
+                new RstSample([
+                    'Title with too short underline',
+                    '~~~~~~~~~~~~~~~~~~~~~~~~',
+                ], 1),
+            ],
+            [
+                Violation::from(
+                    sprintf('Please ensure title "%s" and underline length are matching', 'Title with too long underline'),
+                    'filename',
+                    1,
+                    'Title with too long underline'
+                ),
+                new RstSample([
+                    'Title with too long underline',
+                    '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~',
+                ], 1),
+            ],
+            [
+                NullViolation::create(),
+                new RstSample([
+                    'Title with matching underline',
+                    '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~',
+                ], 1),
+            ],
+        ];
+    }
+}
