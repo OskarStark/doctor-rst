f961c313ae299ee2827c341de7d9179f8839852b
MrYamous <matt.lempereur@gmail.com>
2023-03-09 20:30:09 +0100

    add test for PhpOpenTagInCodeBlockPhpDirective rule

---
+++ tests/Rule/PhpOpenTagInCodeBlockPhpDirectiveTest.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of DOCtor-RST.
+ *
+ * (c) Oskar Stark <oskarstark@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Rule;
+
+use App\Rule\PhpOpenTagInCodeBlockPhpDirective;
+use App\Tests\RstSample;
+use App\Value\NullViolation;
+use App\Value\Violation;
+use App\Value\ViolationInterface;
+
+final class PhpOpenTagInCodeBlockPhpDirectiveTest extends \App\Tests\UnitTestCase
+{
+    /**
+     * @test
+     *
+     * @dataProvider checkProvider
+     */
+    public function check(ViolationInterface $expected, RstSample $sample): void
+    {
+        static::assertEquals(
+            $expected,
+            (new PhpOpenTagInCodeBlockPhpDirective())->check($sample->lines(), $sample->lineNumber(), 'filename')
+        );
+    }
+
+    public function checkProvider(): \Generator
+    {
+        foreach (self::phpCodeBlocks() as $codeBlock) {
+            yield [
+                Violation::from(
+                    sprintf('Please add PHP open tag after "%s" directive', $codeBlock),
+                    'filename',
+                    1,
+                    $codeBlock
+                ),
+                new RstSample([
+                    $codeBlock,
+                    '',
+                    '$this->somePhp();',
+                ]),
+            ];
+
+            yield [
+                NullViolation::create(),
+                new RstSample([
+                    $codeBlock,
+                    '',
+                    '<?php',
+                ]),
+            ];
+        }
+    }
+}
