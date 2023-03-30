b3cc633c313ad5077a63cc129af81ff934104400
Antoine Lamirault <lamiraultantoine@gmail.com>
2023-03-30 13:34:30 +0200

    Add ForbiddenDirectives rule (#1360)

---
+++ src/Rule/ForbiddenDirectives.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of DOCtor-RST.
+ *
+ * (c) Oskar Stark <oskarstark@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Rule;
+
+use App\Annotations\Rule\Description;
+use App\Rst\RstParser;
+use App\Traits\DirectiveTrait;
+use App\Value\Lines;
+use App\Value\NullViolation;
+use App\Value\RuleGroup;
+use App\Value\Violation;
+use App\Value\ViolationInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * @Description("Make sure forbidden directives are not used")
+ */
+class ForbiddenDirectives extends AbstractRule implements Configurable, LineContentRule
+{
+    use DirectiveTrait;
+
+    /**
+     * @var array<string>
+     */
+    private array $forbiddenDirectives;
+
+    public function configureOptions(OptionsResolver $resolver): OptionsResolver
+    {
+        $resolver
+            ->setRequired('directives')
+            ->setAllowedTypes('directives', 'string[]')
+            ->setDefault('arguments', []);
+
+        return $resolver;
+    }
+
+    public function setOptions(array $options): void
+    {
+        $resolver = $this->configureOptions(new OptionsResolver());
+
+        $resolvedOptions = $resolver->resolve($options);
+
+        $this->forbiddenDirectives = $resolvedOptions['directives'];
+    }
+
+    public static function getGroups(): array
+    {
+        return [RuleGroup::Symfony()];
+    }
+
+    public function check(Lines $lines, int $number, string $filename): ViolationInterface
+    {
+        $lines->seek($number);
+        $line = $lines->current();
+
+        foreach ($this->forbiddenDirectives as $forbiddenDirective) {
+            if (RstParser::directiveIs($line, $forbiddenDirective)) {
+                $message = sprintf('Please don\'t use directive "%s" anymore', $line->raw()->toString());
+
+                return Violation::from(
+                    $message,
+                    $filename,
+                    $number + 1,
+                    $line,
+                );
+            }
+        }
+
+        return NullViolation::create();
+    }
+}
---
+++ tests/Rule/ForbiddenDirectivesTest.php
@@ -0,0 +1,103 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of DOCtor-RST.
+ *
+ * (c) Oskar Stark <oskarstark@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Rule;
+
+use App\Rule\ForbiddenDirectives;
+use App\Tests\RstSample;
+use App\Value\NullViolation;
+use App\Value\Violation;
+use App\Value\ViolationInterface;
+use Symfony\Component\OptionsResolver\Exception\InvalidOptionsException;
+
+final class ForbiddenDirectivesTest extends \App\Tests\UnitTestCase
+{
+    /**
+     * @test
+     *
+     * @dataProvider checkProvider
+     */
+    public function check(ViolationInterface $expected, RstSample $sample): void
+    {
+        $rule = new ForbiddenDirectives();
+        $rule->setOptions([
+            'directives' => [
+                '.. index::',
+                '.. caution::',
+            ],
+        ]);
+
+        self::assertEquals(
+            $expected,
+            $rule->check($sample->lines(), $sample->lineNumber(), 'filename'),
+        );
+    }
+
+    /**
+     * @return \Generator<array{0: ViolationInterface, 1: RstSample}>
+     */
+    public static function checkProvider(): \Generator
+    {
+        yield [
+            Violation::from(
+                'Please don\'t use directive ".. index::" anymore',
+                'filename',
+                1,
+                '.. index::',
+            ),
+            new RstSample([
+                '.. index::',
+            ]),
+        ];
+
+        yield [
+            Violation::from(
+                'Please don\'t use directive ".. caution::" anymore',
+                'filename',
+                1,
+                '.. caution::',
+            ),
+            new RstSample([
+                '.. caution::',
+            ]),
+        ];
+
+
+        yield [
+            NullViolation::create(),
+            new RstSample([
+                '.. tip::',
+            ]),
+        ];
+
+        yield [
+            NullViolation::create(),
+            new RstSample('temp'),
+        ];
+    }
+
+    /**
+     * @test
+     */
+    public function invalidOptionType(): void
+    {
+        $this->expectExceptionObject(
+            new InvalidOptionsException('The option "directives" with value ".. caution::" is expected to be of type "string[]", but is of type "string".'),
+        );
+
+        $rule = new ForbiddenDirectives();
+        $rule->setOptions([
+            'directives' => '.. caution::'
+        ]);
+    }
+}
