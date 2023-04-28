b173d1a5ddbac46323c1448b6b0d7d7cf678ac17
Antoine Lamirault <lamiraultantoine@gmail.com>
2023-04-28 20:25:38 +0200

    Add RemoveTrailingWhitespace rule

---
+++ src/Rule/RemoveTrailingWhitespace.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of DOCtor-RST.
+ *
+ * (c) Oskar Stark <oskarstark@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Rule;
+
+use App\Attribute\Rule\Description;
+use App\Attribute\Rule\InvalidExample;
+use App\Attribute\Rule\ValidExample;
+use App\Value\Lines;
+use App\Value\NullViolation;
+use App\Value\RuleGroup;
+use App\Value\Violation;
+use App\Value\ViolationInterface;
+
+#[Description('Make sure there is not trailing whitespace.')]
+#[InvalidExample('Invalid sentence ')]
+#[ValidExample('Valid sentence')]
+class RemoveTrailingWhitespace extends AbstractRule implements LineContentRule
+{
+    public static function getGroups(): array
+    {
+        return [RuleGroup::Symfony()];
+    }
+
+    public function check(Lines $lines, int $number, string $filename): ViolationInterface
+    {
+        $lines->seek($number);
+        $line = $lines->current();
+
+        if ($line->raw()->match('/[a-zA-Z] $/')) {
+            return Violation::from(
+                'Please remove trailing whitespace',
+                $filename,
+                $number + 1,
+                $line,
+            );
+        }
+
+        return NullViolation::create();
+    }
+}
