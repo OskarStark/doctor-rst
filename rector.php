9ebcb497e68f86d1b09010ca684b3fbb4a92c3f1
Oskar Stark <oskarstark@googlemail.com>
2023-09-20 16:10:24 +0200

    Enhancement: Add `rector/rector`

---
+++ rector.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+use Rector\Caching\ValueObject\Storage\FileCacheStorage;
+use Rector\Config\RectorConfig;
+use Rector\Core\ValueObject\PhpVersion;
+use Rector\Doctrine\Set\DoctrineSetList;
+use Rector\Php74\Rector\FuncCall\ArraySpreadInsteadOfArrayMergeRector;
+use Rector\Php80\Rector\Class_\AnnotationToAttributeRector;
+use Rector\PHPUnit\CodeQuality\Rector\Class_\PreferPHPUnitThisCallRector;
+use Rector\PHPUnit\CodeQuality\Rector\ClassMethod\ReplaceTestAnnotationWithPrefixedFunctionRector;
+use Rector\PHPUnit\PHPUnit100\Rector\Class_\StaticDataProviderClassMethodRector;
+use Rector\PHPUnit\Rector\Class_\PreferPHPUnitSelfCallRector;
+use Rector\PHPUnit\Set\PHPUnitLevelSetList;
+use Rector\PHPUnit\Set\PHPUnitSetList;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+use Rector\Symfony\Set\SymfonyLevelSetList;
+use Rector\Symfony\Set\SymfonySetList;
+use Rector\Symfony\Set\TwigLevelSetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->parallel();
+    $rectorConfig->paths([
+        __DIR__.'/.php-cs-fixer.dist.php',
+        __DIR__.'/rector.php',
+        __DIR__.'/src',
+        __DIR__.'/tests',
+    ]);
+
+    $rectorConfig->phpVersion(PhpVersion::PHP_82);
+    $rectorConfig->importNames();
+    $rectorConfig->importShortClasses(false);
+    $rectorConfig->phpstanConfig(getcwd().'/phpstan.neon.dist');
+
+    $rectorConfig->sets([
+        SetList::PHP_82,
+        LevelSetList::UP_TO_PHP_82,
+        PHPUnitLevelSetList::UP_TO_PHPUNIT_90,
+        PHPUnitSetList::PHPUNIT_91,
+        PHPUnitSetList::PHPUNIT_CODE_QUALITY,
+        SymfonyLevelSetList::UP_TO_SYMFONY_63,
+        SymfonySetList::SYMFONY_CODE_QUALITY,
+        DoctrineSetList::DOCTRINE_CODE_QUALITY,
+        TwigLevelSetList::UP_TO_TWIG_240,
+    ]);
+
+    $rectorConfig->skip([
+        ArraySpreadInsteadOfArrayMergeRector::class,
+        PreferPHPUnitThisCallRector::class,
+        ReplaceTestAnnotationWithPrefixedFunctionRector::class,
+    ]);
+
+    /**
+     * @see https://github.com/rectorphp/rector/blob/master/docs/rector_rules_overview.md#annotationtoattributerector
+     */
+    $rectorConfig->rule(AnnotationToAttributeRector::class);
+    $rectorConfig->rule(PreferPHPUnitSelfCallRector::class);
+
+    /**
+     * @see https://github.com/rectorphp/rector-phpunit/blob/main/docs/rector_rules_overview.md#staticdataproviderclassmethodrector
+     */
+    $rectorConfig->rule(StaticDataProviderClassMethodRector::class);
+
+    $rectorConfig->import(__DIR__.'/vendor/fakerphp/faker/rector-migrate.php');
+};
