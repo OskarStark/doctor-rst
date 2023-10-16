d30f70083f0748d2dfe602eddd44ebc1578d8d2d
Antoine Lamirault <lamiraultantoine@gmail.com>
2023-10-16 12:46:59 +0200

    New rule: Ensure link are at the bottom of the file (#1542)

---
+++ tests/Rule/EnsureLinkBottomTest.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of DOCtor-RST.
+ *
+ * (c) Oskar Stark <oskarstark@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Rule;
+
+use App\Rule\EnsureLinkBottom;
+use App\Tests\RstSample;
+use App\Tests\UnitTestCase;
+use App\Value\NullViolation;
+use App\Value\Violation;
+use App\Value\ViolationInterface;
+
+final class EnsureLinkBottomTest extends UnitTestCase
+{
+    /**
+     * @test
+     *
+     * @dataProvider checkProvider
+     */
+    public function check(ViolationInterface $expected, RstSample $sample): void
+    {
+        self::assertEquals(
+            $expected,
+            (new EnsureLinkBottom())->check($sample->lines, $sample->lineNumber, 'filename'),
+        );
+    }
+
+    /**
+     * @return \Generator<array{0: ViolationInterface, 1: RstSample}>
+     */
+    public static function checkProvider(): iterable
+    {
+        yield [
+            NullViolation::create(),
+            new RstSample('temp'),
+        ];
+
+        yield [
+            NullViolation::create(),
+            new RstSample([
+                '',
+                '.. _`first-link`: https://foo.bar',
+            ], 1),
+        ];
+
+        yield [
+            NullViolation::create(),
+            new RstSample([
+                '',
+                '.. _`first-link`: https://foo.bar',
+                '.. _`second-link`: https://foo.baz',
+            ], 1),
+        ];
+
+        yield [
+            Violation::from(
+                'Please move link definition to the bottom of the page',
+                'filename',
+                2,
+                '.. _`first-link`: https://foo.bar',
+            ),
+            new RstSample([
+                '',
+                '.. _`first-link`: https://foo.bar',
+                'text after link',
+            ], 1),
+        ];
+
+        yield [
+            Violation::from(
+                'Please move link definition to the bottom of the page',
+                'filename',
+                2,
+                '.. _`first-link`: https://foo.bar',
+            ),
+            new RstSample([
+                '',
+                '.. _`first-link`: https://foo.bar',
+                '',
+                'text after link',
+            ], 1),
+        ];
+    }
+}
