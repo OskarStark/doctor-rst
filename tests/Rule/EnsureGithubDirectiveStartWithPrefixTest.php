4d86156da4b157d6e401b7eb26d394c097d8a096
Matthieu Lempereur <matt.lempereur@gmail.com>
2024-03-08 07:26:14 +0100

    Ensure :class: :method: :namespace: starts with prefix

---
+++ tests/Rule/EnsureGithubDirectiveStartWithPrefixTest.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of DOCtor-RST.
+ *
+ * (c) Oskar Stark <oskarstark@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Rule;
+
+use App\Rule\EnsureGithubDirectiveStartWithPrefix;
+use App\Tests\RstSample;
+use App\Tests\UnitTestCase;
+use App\Value\NullViolation;
+use App\Value\Violation;
+use App\Value\ViolationInterface;
+
+final class EnsureGithubDirectiveStartWithPrefixTest extends UnitTestCase
+{
+    /**
+     * @test
+     *
+     * @dataProvider checkProvider
+     */
+    public function check(ViolationInterface $expected, string $prefix, RstSample $sample): void
+    {
+        $rule = (new EnsureGithubDirectiveStartWithPrefix());
+        $rule->setOptions(['min_version' => $prefix]);
+
+        self::assertEquals($expected, $rule->check($sample->lines, $sample->lineNumber, 'filename'));
+    }
+
+    /**
+     * @return \Generator<array{0: ViolationInterface, 1: string, 2: RstSample}>
+     */
+    public static function checkProvider(): iterable
+    {
+        yield [
+            NullViolation::create(),
+            'Symfony',
+            new RstSample('Using :class:`Symfony\\Component\\Cache\\Adapter\\PdoAdapter` is deprecated'),
+        ];
+        yield [
+            NullViolation::create(),
+            'Psr',
+            new RstSample('Implements the interface :class:`Psr\\Cache\\CacheItemPoolInterface`'),
+        ];
+        yield [
+            Violation::from(
+                'Please only use Symfony namespace with Github directive',
+                'filename',
+                1,
+                'Implements the interface :class:`Psr\\Cache\\CacheItemPoolInterface`',
+            ),
+            'Symfony',
+            new RstSample('Implements the interface :class:`Psr\\Cache\\CacheItemPoolInterface`'),
+        ];
+    }
+}
