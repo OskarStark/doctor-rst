1799ae3d46a83759a4c106c6c01f5106a4fabdb4
MrYamous <matt.lempereur@gmail.com>
2024-03-21 08:20:16 +0100

    New rule : Ensure correct format for :phpfunction:

---
+++ src/Rule/EnsureCorrectFormatForPhpfunction.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of DOCtor-RST.
+ *
+ * (c) Oskar Stark <oskarstark@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Rule;
+
+use App\Value\Lines;
+use App\Value\NullViolation;
+use App\Value\Violation;
+use App\Value\ViolationInterface;
+
+class EnsureCorrectFormatForPhpfunction extends AbstractRule implements LineContentRule
+{
+
+    public function check(Lines $lines, int $number, string $filename): ViolationInterface
+    {
+        $lines->seek($number);
+        $line = $lines->current();
+
+        if ($line->clean()->match('/:phpfunction:`.*\(\)`/')) {
+
+            return Violation::from(
+                'Please do not use () at the end of PHP function',
+                $filename,
+                $number + 1,
+                $line,
+            );
+        }
+
+        return NullViolation::create();
+    }
+}
