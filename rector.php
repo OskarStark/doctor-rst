fc6a9a42caa930536d92269d5e359e03ef602b96
Oskar Stark <oskarstark@googlemail.com>
2025-02-18 14:14:19 +0100

    Require friendsofphp/php-cs-fixer

--- rector.php
+++ rector.php
@@ -13,7 +13,6 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Doctrine\Set\DoctrineSetList;
-use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
 use Rector\PHPUnit\CodeQuality\Rector\Class_\PreferPHPUnitSelfCallRector;
 use Rector\PHPUnit\CodeQuality\Rector\Class_\PreferPHPUnitThisCallRector;
 use Rector\PHPUnit\CodeQuality\Rector\ClassMethod\ReplaceTestAnnotationWithPrefixedFunctionRector;
