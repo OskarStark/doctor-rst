c87a89b79915d8db896ad1da587b2b0dde26062e
Matthieu Lempereur <matt.lempereur@gmail.com>
2025-11-29 21:07:34 +0100

    New Rule: EnsureAttributeBetweenBackticksInContent (#2061)

---
+++ src/Rule/EnsureAttributeBetweenBackticksInContent.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of DOCtor-RST.
+ *
+ * (c) Oskar Stark <oskarstark@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Rule;
+
+use App\Attribute\Rule\Description;
+use App\Attribute\Rule\InvalidExample;
+use App\Attribute\Rule\ValidExample;
+use App\Rst\RstParser;
+use App\Traits\DirectiveTrait;
+use App\Value\Lines;
+use App\Value\NullViolation;
+use App\Value\Violation;
+use App\Value\ViolationInterface;
+
+#[Description('Make sure to use backticks around attributes in content')]
+#[InvalidExample('Use #[Route] to define route')]
+#[ValidExample('Use ``#[Route]`` to define route')]
+final class EnsureAttributeBetweenBackticksInContent extends AbstractRule implements LineContentRule
+{
+    use DirectiveTrait;
+
+    public function check(Lines $lines, int $number, string $filename): ViolationInterface
+    {
+        $lines->seek($number);
+        $line = $lines->current();
+
+        if ($this->inPhpCodeBlock($lines, $number)) {
+            return NullViolation::create();
+        }
+
+        if ($this->in(RstParser::DIRECTIVE_CODE_BLOCK, $lines, $number, [RstParser::CODE_BLOCK_DIFF])) {
+            return NullViolation::create();
+        }
+
+        if ($line->raw()->match('/(?<!`)#\[[^\]]*\](?!`)/')) {
+            return Violation::from(
+                \sprintf('Please ensure to use backticks "%s"', $line->raw()->toString()),
+                $filename,
+                $number + 1,
+                $line,
+            );
+        }
+
+        return NullViolation::create();
+    }
+}
---
+++ tests/Rule/EnsureAttributeBetweenBackticksInContentTest.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of DOCtor-RST.
+ *
+ * (c) Oskar Stark <oskarstark@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Rule;
+
+use App\Rule\EnsureAttributeBetweenBackticksInContent;
+use App\Tests\RstSample;
+use App\Tests\UnitTestCase;
+use App\Value\NullViolation;
+use App\Value\Violation;
+use App\Value\ViolationInterface;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+
+final class EnsureAttributeBetweenBackticksInContentTest extends UnitTestCase
+{
+    #[Test]
+    #[DataProvider('checkProvider')]
+    public function check(ViolationInterface $expected, RstSample $sample): void
+    {
+        self::assertEquals(
+            $expected,
+            (new EnsureAttributeBetweenBackticksInContent())->check($sample->lines, $sample->lineNumber, 'filename'),
+        );
+    }
+
+    public static function checkProvider(): iterable
+    {
+        foreach (self::phpCodeBlocks() as $codeBlock) {
+            yield \sprintf('No violation for code-block "%s"', $codeBlock) => [
+                NullViolation::create(),
+                new RstSample([
+                    $codeBlock,
+                    '#[AsEventListener]',
+                ]),
+            ];
+        }
+
+        yield 'No violation for diff code-block' => [
+            NullViolation::create(),
+            new RstSample([
+                '.. code-block:: diff',
+                '',
+                '    #[AsEventListener]',
+            ]),
+        ];
+
+        yield 'Has violation without backticks' => [
+            Violation::from(
+                'Please ensure to use backticks "use #[MapEntity] attributes"',
+                'filename',
+                1,
+                'use #[MapEntity] attributes',
+            ),
+            new RstSample('use #[MapEntity] attributes'),
+        ];
+
+        yield 'Has no violation' => [
+            NullViolation::create(),
+            new RstSample('use ``#[MapEntity]`` attributes'),
+        ];
+    }
+}
