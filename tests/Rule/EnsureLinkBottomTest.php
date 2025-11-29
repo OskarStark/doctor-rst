27547a793109a453249bf3aab82e1ad66094d846
Oskar Stark <oskarstark@googlemail.com>
2025-11-29 19:19:37 +0100

    Handle link definitions in code-block:: rst differently (#2178)

--- tests/Rule/EnsureLinkBottomTest.php
+++ tests/Rule/EnsureLinkBottomTest.php
@@ -92,5 +92,61 @@ final class EnsureLinkBottomTest extends UnitTestCase
                 'text after link',
             ], 1),
         ];
+
+        // Link at end of RST code block - valid
+        yield 'link at end of rst code block is valid' => [
+            NullViolation::create(),
+            new RstSample([
+                '.. code-block:: rst',
+                '',
+                '    Some RST content',
+                '',
+                '    .. _`example-link`: https://example.com',
+            ], 4),
+        ];
+
+        // Multiple links at end of RST code block - valid
+        yield 'multiple links at end of rst code block are valid' => [
+            NullViolation::create(),
+            new RstSample([
+                '.. code-block:: rst',
+                '',
+                '    Some RST content',
+                '',
+                '    .. _`first-link`: https://foo.bar',
+                '    .. _`second-link`: https://foo.baz',
+            ], 4),
+        ];
+
+        // Link not at end of RST code block - invalid
+        yield 'link not at end of rst code block is invalid' => [
+            Violation::from(
+                'Please move link definition to the bottom of the RST code block',
+                'filename',
+                4,
+                '.. _`example-link`: https://example.com',
+            ),
+            new RstSample([
+                '.. code-block:: rst',
+                '',
+                '    Some RST content',
+                '    .. _`example-link`: https://example.com',
+                '    More content after link',
+            ], 3),
+        ];
+
+        // Link at end of RST code block, content continues outside - valid
+        yield 'link at end of rst code block with content after code block is valid' => [
+            NullViolation::create(),
+            new RstSample([
+                '.. code-block:: rst',
+                '',
+                '    Some RST content',
+                '',
+                '    .. _`example-link`: https://example.com',
+                '',
+                'Content outside code block',
+            ], 4),
+        ];
     }
 }
