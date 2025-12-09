2b60e612c4573ed3ec3d299e8c5a1360f8f88c10
Oskar Stark <oskarstark@googlemail.com>
2025-12-09 09:22:28 +0100

    Enhancement: Require `rector/rector` with version `^2.2.13`
    _Created by [PR-Factory](https://github.com/sensiolabs-de/pr-factory)_ using the following command:
    
    ```bash
    
    /Users/oskar.stark/dev/sensio/pr-factory/bin/console \
       project:composer:require:rector-2213 \
       --delete-branch-if-exists \
       -vv \
       all
    
    ```

--- rector.php
+++ rector.php
@@ -52,9 +52,7 @@ return static function (RectorConfig $rectorConfig): void {
 
     $rectorConfig->skip([
         PreferPHPUnitThisCallRector::class,
-        ReplaceTestAnnotationWithPrefixedFunctionRector::class,
-        'Rector\Transform\Rector\Assign\PropertyFetchToMethodCallRector', // Deprecated rule in Rector 2.2.6
-    ]);
+        ReplaceTestAnnotationWithPrefixedFunctionRector::class, ]);
 
     $rectorConfig->rule(PreferPHPUnitSelfCallRector::class);
 
